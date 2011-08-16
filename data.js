6647d8837e20acf2569af43d5112593b2921ee40
alexander petro <alexp_alib@hotmail.com>
2011-08-16 17:35:35 +0200

    Pins/ birds profile/ area profile
    title says it all

---
+++ data.js
@@ -0,0 +1,30 @@
+var birds = {
+	'blauwe_kiekendief': {
+		'name': "Blauwe Kiekendief",
+		'image': "blauwe_kiekendief.jpeg",
+		'description': "De Blauwe Kiekendief heeft zijn nest op de grond. Vaak in hoge vegetatie die bescherming moet bieden tegen de vos. Voedselbronnen van deze vogel is het konijn, zangvogels en muizen. De gebieden waarin deze vogel het beste kan jagen zijn landschappen met vooral lage vegetatie, dit betekent gemaaide hooilanden, moerassen, en eventueel randgebieden van meren. Blauwe Kiekendiefen zijn het meest te bezichtigen in de winter tijd.",
+		'locations': ['texel']
+	},
+	'bontbekplevier': {
+		'name': "Bontbekplevier",
+		'image': "Bontbekplevier.jpg",
+		'description': "Dit is een vogel die door het hele jaar kan worden gespot. Dit soort is bekent als een trekvogel dus zijn ze in vrij grote aantallen te zien. Hun voedselbronnen zijn schelpdieren, wormen en andere kleine bodembewoners, dus hun locatie is meestal in open gebieden zoals langs de kust en rivierestuaria.",
+		'locations': ['texel']
+	}	
+}
+
+var locations = {
+	'texel': {
+		'name': "Duinen en Lage Land Texel",
+		'latitude': 53.0255323,
+		'longitude': 4.7511124,
+		'birds': ['blauwe_kiekendief']
+	},
+    'zwanen': {
+		'name': "Zwanenwater & Pettemerduinen",
+		'latitude': 52.8333901, 
+		'longitude': 4.6972176,
+		'birds': []
+	}
+
+}
